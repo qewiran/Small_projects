aa4f7507aa34fc3e853dfc0260c3b02534fc26d0
kirill semyonov <93746347+qewiran@users.noreply.github.com>
2024-02-06 22:40:50 +0300

    trying to get code highlighting

--- num_methods/coordinate_descent/dychotomy.m
+++ num_methods/coordinate_descent/dychotomy.m
@@ -1,16 +1,19 @@
 function point = dychotomy(f, A, B, fileId)
     countOper = 0;
     countIter = 0;
+   
     a = A;
     b = B;
+    
     printTableHeader("dychotomy", fileId);
     point = (b - a) / 2;
-
+   
     while point > 0.0005
         countIter = countIter + 1;
         mid = (a + b) / 2;
         c = mid -2.5e-4;
         d = mid +2.5e-4;
+        
         printTableRow(fileId, countIter, a, b, c, d, f(c), f(d), (b - a) / 2, countOper);
         
         if (f(c) < f(d))
@@ -18,9 +21,8 @@ function point = dychotomy(f, A, B, fileId)
         else
             a = c;
         end
-
+        
         point = (b - a) / 2;
         countOper = countOper + 2;
     end
-
 end
