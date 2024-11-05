e8fb3c3fe364c39ef9fb8e893b5a261121d53d18
qewiran <baldhill228@gmail.com>
2024-11-05 14:42:56 +0300

    add: helpful comments

--- my_BLAS/src/main.cpp
+++ my_BLAS/src/main.cpp
@@ -1,16 +1,9 @@
-// #include "../hdr/iter_method.hpp"
-// #include "../hdr/LUdecomp.hpp"
 #include "../hdr/matrix.hpp"
-// #include "../hdr/QRdecomp.hpp"
-#include "../hdr/power_method.hpp"
-#include <chrono>
-#include <thread>
+#include <iostream>
 int main() {
-  using namespace std::chrono_literals;
-  Matrix<double, 3, 3> a{};
-  Matrix<double, 3, 1> b{};
+  Matrix<double, 3, 3> a;
+  Matrix<double, 3, 1> b;
+  std::cout<<a<<'\n'<<b<<'\n';
   a.makeRandom(0);
-  std::this_thread::sleep_for(1000ms);
   b.makeRandom(0);
-  auto result = directMethod(a, b);
 }
